6d2dea8be6f700bde1d5a519656aa5fd94e705cb
Mikael Joensuu <91052332+MiksuMaker@users.noreply.github.com>
2023-11-16 10:34:50 +0200

    add: Passive pull towards center of pull

--- Assets/_Scripts/Player/Climber.cs
+++ Assets/_Scripts/Player/Climber.cs
@@ -6,25 +6,43 @@ public class Climber : MonoBehaviour
 {
     #region Properties
     Grabber grabber;
+    Rigidbody rb;
+
+    [Header("Climbing Specs")]
+    [SerializeField] float passivePullStrength = 5f;
     #endregion
 
     #region Setup
     private void Start()
     {
         grabber = GetComponent<Grabber>();
+        rb = GetComponent<Rigidbody>();
     }
     #endregion
 
     private void FixedUpdate()
     {
         // Check grab status
-        
+        if (grabber.isClimbing)
+        {
+            PullTowardsCenterOfGravity();
+        }
     }
 
     #region Climbing
-    private void UpdateGrabStatus()
+    private void PullTowardsCenterOfGravity()
     {
+        // Calculate the direction of pull
+        Vector3 pullDir = grabber.center - transform.position;
+        Debug.DrawRay(transform.position, pullDir, Color.yellow, 2f);
 
+        // Pull the Player towards the center
+        rb.MovePosition(transform.position + pullDir * Time.fixedDeltaTime);
     }
     #endregion
+
+    private void OnDrawGizmos()
+    {
+        // Draw a sphere around
+    }
 }
